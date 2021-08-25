5412ada3bffe36cc2570f7bbdf8a9035c703132c
fadhilr <fadilrizqullah97@gmail.com>
2021-08-25 21:40:57 +0700

    Tambah assertion

--- cypress/integration/practice/practice-3.spec.js
+++ cypress/integration/practice/practice-3.spec.js
@@ -2,8 +2,8 @@ describe('Kumpulan test case', () => {
 
     it('TC-1', () => {
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
-        cy.get('#et_pb_contact_name_0').type('Fadhil R')
-        cy.get('#et_pb_contact_email_0').type('fadilrizqullah@gmail.com')
+        cy.get('#et_pb_contact_name_0').type('Fadhil R').should('have.value', 'Fadhil R')
+        cy.get('#et_pb_contact_email_0').type('fadilrizqullah@gmail.com').should('have.value', 'fadilrizqullah@gmail.com')
         
     })
     it('TC-2', () => {
@@ -12,17 +12,17 @@ describe('Kumpulan test case', () => {
         
     })
     it('TC-3', () => {
-        cy.get('[value="male"]').click()
+        cy.get('[value="male"]').click().should('be.checked')
         
     })
     it('TC-4', () => {
-        cy.get('[value="Bike"]').click()
-        cy.get('[value="Car"]').click()
+        cy.get('[value="Bike"]').click().should('be.checked')
+        cy.get('[value="Car"]').click().should('be.checked')
         
     })
     it('TC-5', () => {
         cy.get('select').select('Audi')
-        
+        cy.get('select').contains('Audi')
     })
     it('TC-6', () => {
         cy.get('.et_pb_cta_0 > .et_pb_button_wrapper > .et_pb_button').click()
